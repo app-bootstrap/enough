e2bfde35eb71b6f6bfde58c803a065d2e1c6aea9
dafeng.xdf <dafeng.xdf@taobao.com>
2014-11-04 00:39:10 +0800

    First Commit

---
+++ examples/todo/javascript/todo.js
@@ -0,0 +1,11 @@
+// todo demo
+'use strict';
+
+(function(global, undefined) {
+  var class1 = enough.Klass({
+    constructor: function() {
+    },
+    render: function() {
+    }
+  });
+})(this);
